61ea697f9d982c4bc10949b5332e4fe59b1b7eea
Lea Oertlin <lea.oertlin@student.uibk.ac.at>
2022-06-02 16:12:35 +0200

    format Date funktion erstellen und testen

--- main.js
+++ main.js
@@ -28,6 +28,19 @@ L.control.scale({
     imperial: false
 }).addTo(map);
 
+// Datum formatieren
+let formatDate = function(date) {
+    return date.toLocaleDateString("de-AT", {
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit"
+      
+    })  + "Uhr";
+
+}
+
+
 // Windvorhersage
 async function loadWind(url) {
     const response = await fetch(url);
@@ -40,6 +53,7 @@ async function loadWind(url) {
     console.log("Echtes Datum Erstellung", forecastDate);
     forecastDate.setHours(forecastDate.getHours() + jsondata[0].header.forecastTime)
     console.log("Echtes Datum Vorhersage", forecastDate);
+    console.log("Vorhersagezeitpunkt", formatDate(forecastDate))
 };
 loadWind("https://geographie.uibk.ac.at/webmapping/ecmwf/data/wind-10u-10v-europe.json");
 
